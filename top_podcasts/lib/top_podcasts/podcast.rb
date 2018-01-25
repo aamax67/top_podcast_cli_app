38fc10dcf1f25a06c87712f0d8d7f9640cd24978
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-25 04:14:54 +0000

    refactored podcast class

---
+++ top_podcasts/lib/top_podcasts/podcast.rb
@@ -0,0 +1,23 @@
+class TopPodcasts::Podcast
+  attr_accessor :name, :rank, :url
+
+  def self.today
+    puts "--------- Top 200 Podcasts ---------"
+    puts <<-DOC
+    1.  This American Life
+    2.  The Moth
+    DOC
+
+    podcast_1 = self.new
+    podcast_1.name = "This American Life"
+    podcast_1.rank = "1"
+    podcast_1.url = "http://toppodcast.com/podcast_feeds/this-american-life-this-american-life/"
+
+    podcast_2 = self.new
+    podcast_2.name = "The Moth"
+    podcast_2.rank = "2"
+    podcast_2.url = "http://toppodcast.com/podcast_feeds/the-moth/"
+
+    [deal_1, deal_2]
+  end
+end
