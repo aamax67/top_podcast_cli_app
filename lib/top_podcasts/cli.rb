68e4ae1f0d1446f97cddc4d872a60930b886ef75
Aaron Lieberman <aaronmaxmusic@gmail.com>
2018-01-29 23:23:22 +0000

    revamped cli

--- lib/top_podcasts/cli.rb
+++ lib/top_podcasts/cli.rb
@@ -2,55 +2,38 @@ class TopPodcasts::CLI
 
   def call
     puts "Welcome to the top 200 podcasts:"
-    start
     puts ""
+    list_podcasts
+    puts ""
+    menu
+    goodbye
   end
 
-  def start
-    puts ""
-    puts "What number podcasts would you like to see? 1-50, 51-100, 101-150 or 151-200?"
-    input = gets.strip.to_i
+  def list_podcasts
+    puts "Today's Top 200 Podcasts"
+    @podcasts = TopPodcasts::Podcast.today
+    @podcasts.each.with_index(1) do |podcast, i|
+      puts "#{i}. #{podcast.name}"
+    end
+  end
 
-    print_podcasts(input)
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the podcast you would like more info on or type list to return to list. Or type exit to terminate."
+      input = gets.strip.downcase
 
-    puts ""
-    puts "What podcast would you like more information on?"
-    input = gets.strip
-
-    podcast = TopPodcasts::Podcast.find(input.to_i)
-
-    print_podcast(podcast)
-
-    puts ""
-    puts "Would you like to see another podcast? Enter Y or N"
-
-    input = gets.strip.downcase
-      if input == "y"
-          start
+      if input.to_i > 0
+        puts @podcasts[input.to_i-1]
+      elsif input == "list"
+        list_podcasts
       else
-        puts ""
-        puts "Thankyou! Have a great day!"
-        exit
+        puts "Not sure what you are trying to do. Please type list to see podcasts or exit to terminate."
       end
     end
-
-
-  def print_podcast(podcast)
-    puts ""
-    puts "----------- #{podcast.rank} - #{podcast.name} -----------"
-    puts ""
-    puts "----------------------------Summary---------------------------"
-    puts ""
-    puts "#{podcast.summary}"
-    puts ""
   end
 
-  def print_podcasts(from_number)
-    puts ""
-    puts "---------- Podcasts #{from_number} - #{from_number+49} ----------"
-    puts ""
-    TopPodcasts::Podcast.all[from_number-1, 50].each.with_index(from_number) do |podcast, index|
-      puts "#{index}.   #{podcast.rank} - #{podcast.name}"
-    end
+  def goodbye
+    puts "Come back anytime to find your favorite podcast!"
   end
 end
--- lib/top_podcasts/podcast.rb
+++ lib/top_podcasts/podcast.rb
@@ -3,31 +3,35 @@ class TopPodcasts::Podcast
 
     @@all = []
 
+    def self.today
+      self.scrape_podcasts
+    end
+
     def initialize(name = nil, rank = nil, url = nil)
       @name = name
       @rank = rank
       @url = url
       @summary = summary
-
     end
 
     def self.all
-      @@all << self.scrape_podcast
+
+      podcasts << self.scrape_podcasts
+      @@all << podcasts
     end
 
     def self.find(id)
       self.all[id-1]
     end
 
-  def self.scrape_podcast
-
+  def self.scrape_podcasts
+    podcasts = []
     doc = Nokogiri::HTML(open("http://toppodcast.com/top-200-podcast/"))
-    binding.pry
     podcast = self.new
-    podcast.name = doc.search(".podcastBlock podcastBlock3").css("h3").text.gsub(/\t/, '').strip
-    podcast.rank = doc.search(".podcastRow").css(".numberImage".text.gsub(/\n/, '').strip
-    podcast.summary = doc.search(".podcastRow").css("p").first.text.strip
+    podcast.name = doc.search(".podcastRow").css("h3").text.gsub(/\t/, '').strip
+    podcast.rank = doc.search(".podcastRow").css(".numberImage").text.gsub(/\t/, '').strip
+    podcast.summary = doc.search(".podcastRow").css("p").text.strip
     podcast.url = doc.search("a.view_show").first.attr("href")
-    podcast
+    podcasts
   end
 end
